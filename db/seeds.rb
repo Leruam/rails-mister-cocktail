0eb23c966068777540face00fe0044d62394dc71
antoine maurel <antoine.maurel@gmail.com>
2020-11-12 14:23:34 +0100

    done seed with uri

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'json'
+require 'open-uri'
+
+url = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredients_series = open(url).read
+drinks_obj = JSON.parse(ingredients_series)
+ingredients_tab = drinks_obj["drinks"].flatten
+
+10.times do |variable|
+  Ingredient.create(name: ingredients_tab[rand(0..(ingredients_tab.length - 1))]["strIngredient1"])
+end
+
