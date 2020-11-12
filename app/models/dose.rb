ed9852daedb271b94469d5feac0086b7beef4316
antoine maurel <antoine.maurel@gmail.com>
2020-11-12 12:18:37 +0100

    bug

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,8 +1,2 @@
 class Dose < ApplicationRecord
-  belongs_to :cocktail, dependent: :destroy
-  belongs_to :ingredient
-
-  # A dose must have a description, a cocktail and an ingredient, and [cocktail, ingredient] pairings should be unique.
-  validates :description, presence: true
-  validates :name, presence: true
 end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,5 +1,2 @@
 class Ingredient < ApplicationRecord
-  has_many :doses
-  validates :name, uniqueness: true
-  # You can’t delete an ingredient if it is used by at least one cocktail.
 end
